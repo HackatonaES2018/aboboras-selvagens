7a6729141a4102d41fd67e9143f95d87f0e536d8
guilherme.piccoli <guilherme.piccoli@acad.pucrs.br>
2018-11-17 18:24:03 -0200

    clean code

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import {
   Image,
-  Platform,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
 import { TextField } from 'react-native-material-textfield';
-import { MonoText } from '../components/StyledText';
 import Button from '../components/Button';
 
 export default class HomeScreen extends React.Component {
@@ -52,43 +49,6 @@ export default class HomeScreen extends React.Component {
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  onPressLearnMore = () => {
-    const {navigate} = this.props.navigation;
-    navigate('Register')
-  };
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
