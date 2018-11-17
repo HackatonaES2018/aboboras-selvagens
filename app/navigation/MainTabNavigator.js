8693649dc52dbe497943cb4e389b246a0c4391a8
Vinícius Müller Teixeira <vinicius.teixeira.001@acad.pucrs.br>
2018-11-17 16:47:39 -0200

    inicio carteira digital

--- app/navigation/MainTabNavigator.js
+++ app/navigation/MainTabNavigator.js
@@ -4,8 +4,10 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import MainScreen from '../screens/Wallet/MainScreen';
 
 const LoginStack = createStackNavigator({
+  Wallet_MainScreen: MainScreen,
   Login: LoginScreen,
   Register: RegisterScreen
 });
