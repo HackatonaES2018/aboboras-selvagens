87cdf9ed9da70206f77db54ab276a2c5a551846c
fernando.maioli <fernando.maioli@acad.pucrs.br>
2018-11-17 18:41:01 -0200

    initial screen ugly

--- app/navigation/MainTabNavigator.js
+++ app/navigation/MainTabNavigator.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
+import InitialScreen from '../screens/InitialScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import SimularScreen from '../screens/SimularScreen';
 
 const LoginStack = createStackNavigator({
+  Initial: InitialScreen,
   Login: LoginScreen,
   Register: RegisterScreen,
   Simular: SimularScreen
 }, {
-    initialRouteName: 'Login'
+    initialRouteName: 'Initial'
   });
 
 
